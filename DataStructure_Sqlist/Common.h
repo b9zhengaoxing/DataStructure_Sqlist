a396e0b3697bb22b9a57bdceefb0fe3a6682beac
b9zhengaoxing <1164834201@qq.com>
2015-09-10 14:04:47 +0800

    线性表的顺序存储和表示

---
+++ DataStructure_Sqlist/Common.h
@@ -0,0 +1,23 @@
+//
+//  Common.h
+//  DataStructure_Sqlist
+//
+//  Created by tengfei li on 10/9/15.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#ifndef DataStructure_Sqlist_Common_h
+#define DataStructure_Sqlist_Common_h
+
+//定义状态位
+
+#define TRUE	    1
+#define FALSE		0
+#define OK			1
+#define ERROR		0
+#define INFEASIBLE  -1
+#define OVERFLOW	-2
+
+typedef int Status;
+
+#endif
